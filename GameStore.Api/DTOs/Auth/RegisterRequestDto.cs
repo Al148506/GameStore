d51c505a00d20cca09bc6fc0e27dc41a8ad8d3da
Al148506 <al148506@gmail.com>
2025-10-18 18:55:55 -0600

    Frontend: Funcionalidad para la pagina de registro terminada

--- GameStore.Api/DTOs/Auth/RegisterRequestDto.cs
+++ GameStore.Api/DTOs/Auth/RegisterRequestDto.cs
@@ -6,10 +6,10 @@ namespace GameStore.Api.Dtos.Auth
     {
         [Required(ErrorMessage = "El correo es obligatorio.")]
         [EmailAddress(ErrorMessage = "El correo no tiene un formato válido.")]
-        public string Email { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "La contraseña es obligatoria.")]
         [StringLength(50, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
-        public string Password { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
     }
 }

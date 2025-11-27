0c18527e20359bd1b5c10ad0299fd17dcdbc66ac
Al148506 <al148506@gmail.com>
2025-11-27 14:34:59 -0600

    Unit tests for GamesController

---
+++ GameStore.Tests/Mapper/TestMapperFactory.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using GameStore.Api.DTOs.Videogames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Tests.Mapper
+{
+    public class TestMapperFactory
+    {
+        public static IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(typeof(VideogameDTO));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
